59e51d372fd2f00b76b211143ce8a9161e15fee2
Alamin Shaikh <hello@octib.com>
2023-08-01 11:39:39 +0600

    Created and integrated getMostFrequentPhrases function

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,47 @@
 import puppeteer from 'puppeteer';
 
+interface IPhrases {
+  [key: string]: number;
+}
+
 // Get most frequent phrases
+function getMostFrequentPhrases(input: string, outputCount: number) {
+  // Remove everything that's not a word or a white space
+  // convert the text to lower case
+  const cleanedText = input.replace(/[^\w\s]/g, '').toLowerCase();
+
+  // Create array of words
+  // Split by one or more white space
+  const words = cleanedText.split(/\s+/);
+
+  // Create phrases object
+  let phrases: IPhrases = {};
+
+  // Loop through the words
+  for (let i = 0; i < words.length; i++) {
+    // For each word create a phrase that is 2-3 words long
+    for (let j = 2; j < 4; j++) {
+      // Create a phrase
+      const phrase = words.slice(i, i + j).join(' ');
+
+      // Update phrases
+      phrases = { ...phrases, [phrase]: (phrases[phrase] || 0) + 1 };
+    }
+  }
+
+  // Return the most frequent phrases
+  return (
+    Object.entries(phrases)
+      // Sort the phrases by most frequency
+      .sort((a, b) => b[1] - a[1])
+      // Get a certain number of phrases
+      .slice(0, outputCount)
+      // Get the phrases text
+      .map((element) => element[0])
+      // Remove empty phrases
+      .filter((phrase) => phrase.trim())
+  );
+}
 
 // Extract keyword
 async function extractKeywords(url: string) {
@@ -49,7 +90,12 @@ async function extractKeywords(url: string) {
     // Create input
     const input = `${data.title} ${data.description} ${data.heading}`;
 
-    console.log(input);
+    // Get most frequently used phrases
+    const mostFrequentlyUsedPhrases = getMostFrequentPhrases(input, 5);
+
+    // Log and return the phrases
+    console.log(mostFrequentlyUsedPhrases);
+    return mostFrequentlyUsedPhrases;
   } catch (err) {
     // Log error
     console.log(err);
